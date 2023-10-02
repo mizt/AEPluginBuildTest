6b80e443dae4a4b054291f1d9e4091a8f3c07855
mizt <info@mizt.org>
2023-10-02 23:45:36 +0900

    Update

--- Effect/MSL/MSL.sh
+++ Effect/MSL/MSL.sh
@@ -9,10 +9,10 @@ fi
 
 mkdir ../$1
 
-sed -e "s/PLUGIN=\"PLUGIN\"/PLUGIN=\"$1\"/g" ./build.sh >> ../$1/build.sh
+sed -e "s/PLUGIN=\"PLUGIN\"/PLUGIN=\"$1\"/g" ./_build.sh >> ../$1/build.sh
 sed -e "s/#define MFR Plugin/#define MFR $1/g" \
 -e "s/#define IDENTIFIER @\"Ae.MSL.Plugin\"/#define IDENTIFIER @\"Ae.MSL.$1\"/g" \
--e "s/#define METALLIB @\"Plugin.metallib\"/#define METALLIB @\"$1.metallib\"/g" ./Config.h >> ../$1/Config.h
+-e "s/#define METALLIB @\"Plugin.metallib\"/#define METALLIB @\"$1.metallib\"/g" ./_Config.h >> ../$1/Config.h
 cp ./default.metal ../$1/$1.metal
 sed -e "s/\<string\>Plugin\<\/string\>/\<string\>$1\<\/string\>/g" \
 -e "s/\<string\>Ae.MSL.Plugin\<\/string\>/\<string\>Ae.MSL.$1\<\/string\>/g" \
